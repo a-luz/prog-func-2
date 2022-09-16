1797c57ed1aa0e0ed31631011b76a4445fec1976
Elton Peixoto <elton.peixoto@nubank.com.br>
2022-09-16 18:01:07 -0300

    subindo aula

---
+++ src/maps/exercicio.clj
@@ -0,0 +1,40 @@
+(ns maps.exercicio
+ (:require [ maps.data :refer :all]) )
+
+
+;; dado o mapa no namespace data.clj, escrever funcoes que:
+;; altera a idade do cliente
+;; remove o endereco do cliente
+;; atualiza o valor de um item de compra
+;; adicionar o vetor de compras no mapa do cliente
+;; adiciona um novo item de compra
+;; retorna o nome do cliente
+
+
+(defn altera-idade
+  [cliente idade]
+  (assoc cliente :idade idade))
+
+
+(defn remove-endereco
+  [cliente]
+  (dissoc cliente :endereco ))
+
+
+(defn atualiza-compra
+  [compra novo-valor ]
+  (assoc compra :valor novo-valor ))
+
+(defn adciona-vetor
+  [cliente compras ]
+  (assoc cliente :compras compras ))
+
+
+(defn adcionaitem-compra
+  [item novo-item ]
+  (assoc item :item novo-item ))
+
+
+(defn retorna-nome
+  [cliente]
+  (:nome cliente))
