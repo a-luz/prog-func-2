8384b64fad0d99e60e32caa136645f5106d04040
a-luz <arthurg.luz@gmail.com>
2022-09-21 16:40:09 -0300

    aula 3

--- src/aula3/exercicio.clj
+++ src/aula3/exercicio.clj
@@ -1,1 +1,9 @@
 (ns aula3.exercicio)
+
+;; escrever uma funcao que retorna a primeira compra do cliente dado o ID.
+
+;; escrever uma funcao que retorna as compras de um cliente dado o ID.
+
+;; escrever uma funcao que adiciona a compra {:item "mesa" :valor 250} no vetor de compras de um cliente, dado o ID. Deve retornar o mapa inteiro.
+
+;; escrever uma funcao que da desconto de 10% para o primeiro item de compra do cliente dado o ID. Deve retornar o mapa inteiro
